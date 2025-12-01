d52d05ab0b445fbee7c1754b1806e80b3249b227
Arthur Henrique Marinho <ykhalloween4@gmail.com>
2025-11-30 22:03:20 -0300

    Export nas classes do model

--- demo/baseBackend/model/modeloCracha.js
+++ demo/baseBackend/model/modeloCracha.js
@@ -1,12 +1,14 @@
-// JavaScript não possui Enums de forma nativa, vou "simular".
-const TIPO_EMOCAO = {
+// JavaScript não possui Enums de forma nativa, vou "simular"
+// Alterei para ser possível dar imports no futuro tanto nos tipos de emoção quanto na class.
+
+export const TIPO_EMOCAO = {
   FELIZ: "feliz",
   NEUTRO: "neutro",
   TRISTE: "triste"
 };
 
 //Classe para representar o modelo lógico que fiz a anotação:
-class RegistroEmocao {
+export class RegistroEmocao {
   /**
    * @param {number} id -identificador único
    * @param {string} emocao - Valor da emoção
@@ -19,9 +21,11 @@ class RegistroEmocao {
       throw new Error("Emoção inválida: ${emocao}. Esperado: ${Object.values(TIPO_EMOCAO).join(",")}");
     }
     this.emocao = emocao;
-    this.timestamp = new Date(timestamp);
+    
+    //Garante a instância de Date
+    this.timestamp = timestamp instanceof Date ? timestamp : new Date(timestamp);
   }
-  //Como foi comentado de usar arquivo JSON, vou deixar aqui um método útil para formatar dados para o dashboard (se não precisarem, so comentar o código)
+  //Como foi comentado de usar arquivo JSON, vou deixar aqui um método útil para formatar dados para o dashboard (se não precisarem, so comentar o código). A função do JSON tá na classe, então tá tranquilo.
   toJSON() {
     return {
       id: this.id,
